e392c391723fb69d75db462938ac0d9518b3f834
Aldo Ulises <aldo@MacBook-Pro-de-Aldo.local>
2013-09-17 12:54:15 -0500

    Initial Commit

---
+++ AppDemoTests/AppDemoTests.h
@@ -0,0 +1,13 @@
+//
+//  AppDemoTests.h
+//  AppDemoTests
+//
+//  Created by Aldo Ulises on 17/09/13.
+//  Copyright (c) 2013 Aldo Ulises. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AppDemoTests : SenTestCase
+
+@end
---
+++ AppDemoTests/AppDemoTests.m
@@ -0,0 +1,32 @@
+//
+//  AppDemoTests.m
+//  AppDemoTests
+//
+//  Created by Aldo Ulises on 17/09/13.
+//  Copyright (c) 2013 Aldo Ulises. All rights reserved.
+//
+
+#import "AppDemoTests.h"
+
+@implementation AppDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in AppDemoTests");
+}
+
+@end
